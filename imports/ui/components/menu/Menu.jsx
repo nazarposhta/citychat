a2778720328e65ecee145f0ffc6ffad468099267
Nazariy Snihur <nsnihur@linguahouse.com>
2021-11-23 21:26:45 +0100

    Bootstrap font and logo

--- imports/ui/components/menu/Menu.jsx
+++ imports/ui/components/menu/Menu.jsx
@@ -3,8 +3,6 @@ import { Meteor } from 'meteor/meteor'
 import { Link } from 'react-router-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 
-import "./Menu.scss";
-
 export default withTracker(() => {
     return {
         userId: Meteor.userId()
@@ -14,27 +12,41 @@ export default withTracker(() => {
     return(
         <header className="menu">
             <div className="container">
+                <Link to="/">
+                    <span className="logo">
+                        <b>Live</b>
+                        <i className="bi bi-qr-code-scan"></i>
+                        <b>menU</b>
+                    </span>
+                </Link>
                 <div className="navbar clearfix">
                     <ul className="nav">
                         <li className="nav-item">
-                            <Link className="nav-link active" to="/">Home</Link>
+                            <Link className="nav-link" to="/create-account">About us</Link>
                         </li>
-                        {
-                            userId ?
-                                <a className="nav-link" onClick={() => {Meteor.logout()}}>
-                                    Logout
-                                </a>
-                                :
-                                <React.Fragment>
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to="/create-account">Create account</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to="/login">Login</Link>
-                                    </li>
-                                </React.Fragment>
-                        }
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/create-account">Pricing</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/create-account">Contact</Link>
+                        </li>
                     </ul>
+                </div>
+                <div className="sign-up">
+                    {
+                        userId ?
+                            <button type="button"
+                                    className="btn btn-primary"
+                                    onClick={() => {Meteor.logout()}}
+                            >
+                                Logout
+                            </button>
+                            :
+                            <>
+                                <Link className="btn btn-primary" to="/create-account">Sign-up</Link>
+                                <Link className="btn btn-primary" to="/login">Sign-in</Link>
+                            </>
+                    }
                 </div>
             </div>
         </header>
